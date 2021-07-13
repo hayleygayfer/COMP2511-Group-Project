bf938681f7ee7d4fe335bb2b4e6b0e89490164c3
Jasmin Oren <jasminoren@Jasmins-MacBook-Pro.local>
2021-07-13 13:29:45 +1000

    written teests for encounter buildsing added in heros castle

---
+++ src/unsw/loopmania/buildings/HerosCastleBuilding.java
@@ -0,0 +1,17 @@
+package unsw.loopmania.buildings;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import unsw.loopmania.Building;
+
+public class HerosCastleBuilding extends Building{
+
+    // TODO write Hero's Castle building
+    public HerosCastleBuilding(SimpleIntegerProperty x, SimpleIntegerProperty y) {
+        super(x, y);
+    }
+    
+    public void encounter(Character character) {
+
+    }
+    
+}
