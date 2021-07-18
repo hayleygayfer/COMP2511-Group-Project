bf724af53c2bc5856e4d3c2dd503607679d3900f
Angeni Bai <angenibai@gmail.com>
2021-07-18 20:09:20 +1000

    tower tests

--- src/test/cards/TowerCardTest.java
+++ src/test/cards/TowerCardTest.java
@@ -1,88 +1,74 @@
 package test.cards;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import test.TestHelper;
 
 import org.junit.jupiter.api.Test;
 import org.javatuples.Pair;
-import java.util.ArrayList;
 import java.util.List;
-import unsw.loopmania.LoopManiaWorld;
 
+
+import unsw.loopmania.Building;
+import unsw.loopmania.StaticEntity;
+import unsw.loopmania.Card;
+import unsw.loopmania.EnemyPositionObserver;
+import unsw.loopmania.Entity;
 import unsw.loopmania.cards.TowerCard;
 import unsw.loopmania.buildings.TowerBuilding;
 
 public class TowerCardTest {
-    
-    /**
-     * @author Angeni
-     * Creates the path for testing
-     * just a 5x5 square loop
-     */
-    public List<Pair<Integer, Integer>> createSquarePath(int size, int start) {
-        List<Pair<Integer, Integer>> orderedPath = new ArrayList<>();
-
-        // add top horizontal
-        for (int i = start; i < size; i++) {
-        orderedPath.add(Pair.with(i, start));
-        }
-        // add right side down
-        for (int i = start + 1; i < size; i++) {
-        orderedPath.add(Pair.with(size - 1, i));
-        }
-        // add bottom horizontal
-        for (int i = (size-2); i >= start; i--) {
-        orderedPath.add(Pair.with(i, size-1));
-        }
-        // add left side up
-        for (int i = (size-2); i > start; i--) {
-        orderedPath.add(Pair.with(start, i));
-        }
-        return orderedPath;
-    }
-
-    /**
-     * @author Angeni
-     * @return LoopManiaWorld object
-     */
-    public LoopManiaWorld createWorld() {
-        return new LoopManiaWorld(6, 6, createSquarePath(6, 0));
-    }
-
     @Test
-    public void testGenerateBuilding(){
-        TowerCard card = new TowerCard(new SimpleIntegerProperty(0), new SimpleIntegerProperty(0));
-        TowerBuilding building = new TowerBuilding(new SimpleIntegerProperty(1), new SimpleIntegerProperty(1));
+    public void testConstructor() {
+        Card card = new TowerCard(new SimpleIntegerProperty(2), new SimpleIntegerProperty(3));
 
-        // Tests that the building is generated at the given coordinates.
-        assertEquals(card.generateBuilding(new SimpleIntegerProperty(1), new SimpleIntegerProperty(1)), building);
+        assertTrue(card instanceof TowerCard);
+        assertTrue(card instanceof Card);
+        assertTrue(card instanceof StaticEntity);
+        assertTrue(card instanceof Entity);
+
+        assertEquals(2, card.getX());
+        assertEquals(3, card.getY());
+    }
+    
+    @Test
+    public void testGenerateBuilding() {
+        TowerCard card = new TowerCard(new SimpleIntegerProperty(0), new SimpleIntegerProperty(0));
+
+        Building building = card.generateBuilding(new SimpleIntegerProperty(2), new SimpleIntegerProperty(7));
+
+        assertTrue(building instanceof TowerBuilding);
+        assertEquals(2, building.getX());
+        assertEquals(7, building.getY());
     }
 
     @Test
     public void testValidPosition() {
-        LoopManiaWorld world = createWorld();
-        List<Pair<Integer, Integer>> adjacentPath = createSquarePath(5, 1); // Valid Positions
+        List<Pair<Integer, Integer>> worldPath = TestHelper.createSquarePath(6, 0);
+        List<Pair<Integer, Integer>> adjacentPath = TestHelper.createSquarePath(5, 1); // Valid Positions
 
         TowerCard card = new TowerCard(new SimpleIntegerProperty(0), new SimpleIntegerProperty(0));
 
         // Tests that all adjacent pairs are valid
         for (Pair<Integer, Integer> position: adjacentPath) {
-            assertTrue(card.isValidPosition(new SimpleIntegerProperty(position.getValue0()), new SimpleIntegerProperty(position.getValue1()), world.getPath()));
+            assertTrue(card.isValidPosition(new SimpleIntegerProperty(position.getValue0()), new SimpleIntegerProperty(position.getValue1()), worldPath));
         }
     }
 
-
+    @Test
     public void testInvalidPosition() {
-        LoopManiaWorld world = createWorld();
-        List<Pair<Integer, Integer>> nonAdjacentPath = createSquarePath(4, 2); // Valid Positions
+        List<Pair<Integer, Integer>> worldPath = TestHelper.createSquarePath(6, 0); // Valid Positions
+        List<Pair<Integer, Integer>> nonAdjacentPath = TestHelper.createSquarePath(4, 2); // Valid Positions
+        
 
         TowerCard card = new TowerCard(new SimpleIntegerProperty(0), new SimpleIntegerProperty(0));
 
         // Tests that all non adjacent pairs are invalid
         for (Pair<Integer, Integer> position: nonAdjacentPath) {
-            assertTrue(card.isValidPosition(new SimpleIntegerProperty(position.getValue0()), new SimpleIntegerProperty(position.getValue1()), world.getPath()));
+            assertFalse(card.isValidPosition(new SimpleIntegerProperty(position.getValue0()), new SimpleIntegerProperty(position.getValue1()), worldPath));
         }
     }
 }
--- src/unsw/loopmania/buildings/TowerBuilding.java
+++ src/unsw/loopmania/buildings/TowerBuilding.java
@@ -1,19 +1,21 @@
 package unsw.loopmania.buildings;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import unsw.loopmania.BasicEnemy;
 import unsw.loopmania.Building;
 import unsw.loopmania.CharacterPositionObserver;
+import unsw.loopmania.EnemyPositionObserver;
 import unsw.loopmania.Character;
 import javafx.scene.image.Image;
 import java.io.File;
 
-public class TowerBuilding extends Building implements CharacterPositionObserver {
+public class TowerBuilding extends Building implements EnemyPositionObserver {
     // TODO write tower building
     public TowerBuilding(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
     }
     
-    public void encounter(Character character) {
+    public void encounter(BasicEnemy enemy) {
 
     }
 
