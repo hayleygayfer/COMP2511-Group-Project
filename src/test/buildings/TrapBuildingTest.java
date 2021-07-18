448be2e28ef6e48f61ae5e63095f9efbe3adee90
Angeni Bai <angenibai@gmail.com>
2021-07-18 15:58:42 +1000

    trap building test done

--- src/test/buildings/TrapBuildingTest.java
+++ src/test/buildings/TrapBuildingTest.java
@@ -1,25 +1,72 @@
 package test.buildings;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javafx.beans.property.SimpleIntegerProperty;
+import java.util.List;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import test.TestHelper;
+
+import org.javatuples.Pair;
 import org.junit.jupiter.api.Test;
 
-import unsw.loopmania.BasicEnemy;
-import unsw.loopmania.buildings.CampfireBuilding;
+import unsw.loopmania.Building;
 import unsw.loopmania.buildings.TrapBuilding;
-import unsw.loopmania.enemies.Zombie;
-import unsw.loopmania.Character;
+import unsw.loopmania.enemies.Vampire;
+import unsw.loopmania.StaticEntity;
+import unsw.loopmania.Entity;
+import unsw.loopmania.PathPosition;
 
 public class TrapBuildingTest {
     @Test
-    public void testSpawnEnemy(){
+    public void testConstructor() {
+        Building trap = new TrapBuilding(new SimpleIntegerProperty(0), new SimpleIntegerProperty(3));
 
-      TrapBuilding building = new TrapBuilding(new SimpleIntegerProperty(1), new SimpleIntegerProperty(1));
-      
-      // TODO: Test effect on enemy position
+        assertTrue(trap instanceof TrapBuilding);
+        assertTrue(trap instanceof Building);
+        assertTrue(trap instanceof StaticEntity);
+        assertTrue(trap instanceof Entity);
 
+        assertEquals(0, trap.getX());
+        assertEquals(3, trap.getY());
+    }
+
+    @Test
+    public void testEncounter() {
+        List<Pair<Integer, Integer>> path = TestHelper.createPath();
+        Vampire vampire = new Vampire(new PathPosition(1, path));
+
+        int initialHealth = vampire.getHealth();
+
+        TrapBuilding trap = new TrapBuilding(new SimpleIntegerProperty(0), new SimpleIntegerProperty(2)); 
+
+        vampire.moveDownPath();
+        // vampire is now on same tile as trap
+        trap.encounter(vampire);
+
+        // vampire loses health and trap ceases to exist
+        assertEquals(initialHealth - 3, vampire.getHealth());
+        assertFalse(trap.shouldExist().get());
+    }
+
+    @Test
+    public void testNoEncounter() {
+        List<Pair<Integer, Integer>> path = TestHelper.createPath();
+        Vampire vampire = new Vampire(new PathPosition(1, path));
+
+        int initialHealth = vampire.getHealth();
+
+        TrapBuilding trap = new TrapBuilding(new SimpleIntegerProperty(0), new SimpleIntegerProperty(3)); 
+
+        vampire.moveDownPath();
+        // vampire is now on same tile as trap
+        trap.encounter(vampire);
+
+        // nothing should have changed
+        assertEquals(initialHealth, vampire.getHealth());
+        assertTrue(trap.shouldExist().get()); 
     }
 }
 
